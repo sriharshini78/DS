02f3c156fdbb0424249e2fc3c4766573d013a90f
sriharshini78 <gudipellysriharshini@gmail.com>
2025-09-26 15:38:47 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int n;
+printf("enter the no.of elements :");
+scanf("%d",&n);
+int a[n],i;
+for(i=0;i<n;i++){
+scanf("%d",&a[i]);
+
+printf("Enter the elements :%d\n",a[i]);
+}
+}
